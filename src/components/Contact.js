13ba2b525069236e9fc064344167e31723d08c36
ayubov <alvi.ayubov@yandex.ru>
2018-03-06 16:36:26 +0300

    init

---
+++ src/components/Contact.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Contact extends Component {
+  render() {
+    return (
+<h2 className="rotate">contacts</h2>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+<h2 className="rotate">hello</h2>
+    );
+  }
+}
+
+export default Home;
