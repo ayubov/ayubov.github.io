297a7c501c884511fbbf804ab5e1c424706dd8c4
ayubov <alvi.ayubov@yandex.ru>
2019-11-03 16:13:12 +0300

    add semantic markup, fix content flashes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import injectSheet from "react-jss";
-import avatar from "../media/amsterdam.jpeg";
+import FontFaceObserver from 'fontfaceobserver'; 
+
+import avatarSrc from "../media/amsterdam.jpeg";
 import { ReactComponent as TelegramIcon } from "../icons/telegram.svg";
 import { ReactComponent as GithubIcon } from "../icons/github.svg";
 import { ReactComponent as MailIcon } from "../icons/mail.svg";
 import { ReactComponent as LinkedinIcon } from "../icons/linkedin.svg";
 
 const styles = {
-  container: {
+  main: {
     border: "1px solid black",
     padding: "20px 40px",
-    "& div:not(:last-child)": {
+    "& > *:not(:last-child)": {
       marginBottom: "20px"
     }
   },
@@ -19,13 +21,14 @@ const styles = {
     height: "50%",
     width: "50%"
   },
-  avatarContainer: {
+  avatarSection: {
     display: "flex",
     justifyContent: "center"
   },
   title: {
     fontSize: "3em",
-    textAlign: "center"
+    textAlign: "center",
+    marginTop: 0,
   },
   icons: {
     display: "flex",
@@ -40,24 +43,39 @@ const styles = {
   }
 };
 
-const Home = ({ classes }) => (
-  <div className={classes.container}>
-    <div className={classes.avatarContainer}>
-      <img className={classes.avatar} src={avatar} alt="Alvi Ayubov" />
-    </div>
-    <div className={classes.title}>Alvi Ayubov</div>
-    <div className={classes.textContent}>
-      I'm Alvi. I work as Frontend Developer at NetUP in Moscow. I have a good
+const Home = ({ classes }) => {
+  const [ isAvatarLoaded, setIsAvatarLoaded ] = useState(false);
+  const [ isFontLoaded, setIfFontLoaded ] = useState(false);
+  
+  useEffect(() => {
+    // prevent content flashes
+    const avatar  = new Image;
+    avatar.onload = () => setIsAvatarLoaded(true);
+    avatar.src = avatarSrc;
+
+    const font = new FontFaceObserver('Titillium Web');
+    font.load().then(() => setIfFontLoaded(true));
+  }, [])
+
+  return isAvatarLoaded && isFontLoaded && (
+    <main className={classes.main}>
+    <section className={classes.avatarSection}>
+      <img className={classes.avatar} src={avatarSrc} alt="Alvi Ayubov" />
+    </section>
+    <h1 className={classes.title}>Alvi Ayubov</h1>
+    <article className={classes.textContent}>
+      I'm Alvi. I work as Frontend Developer at Rambler&CO in Moscow. I have a good
       experience at making apps with modern techniques. I love programming,
       reading, sport and travelling.
-    </div>
-    <div className={classes.icons}>
+    </article>
+    <footer className={classes.icons}>
       <GithubIcon />
       <LinkedinIcon />
       <TelegramIcon />
       <MailIcon />
-    </div>
-  </div>
-);
+    </footer>
+  </main>
+  )
+}
 
 export default injectSheet(styles)(Home);
