4d5c6e34a4f788095a3965e893fd1927d825e344
ayubov <alvi.ayubov@yandex.ru>
2019-11-24 15:43:15 +0300

    add tony theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
-import injectSheet from "react-jss";
-import Home from "./components/Home";
-import Gradient from "./gradient/containers/Gradient";
+import React, { useState } from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import { ThemeProvider } from 'react-jss'
 
-const styles = {
-  app: {
-    maxWidth: "700px",
-    margin: "auto"
+import { ThemeContext } from './context'
+import Home from './components/Home'
+import Gradient from './gradient/containers/Gradient'
+
+
+const themes = {
+  alvi: {
+    background: '#ede6d1',
+    color: '#626960',
+    hover: 'rgba(0,0,0,0.2)'
+  },
+  tony: {
+    background: '#a6b2a1',
+    color: '#f1e5e6',
+    hover: 'rgba(255,255,255,0.8)'
   }
-};
+}
 
-const App = ({ classes }) => (
-  <div className={classes.app}>
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/gradient" component={Gradient} />
-    </Router>
-  </div>
-);
+const App = ({ classes }) => {
+  const [theme, setTheme] = useState('alvi');
+  const toggleTheme = () => setTheme(theme => theme === 'alvi' ? 'tony' : 'alvi');
 
-export default injectSheet(styles)(App);
+  return (
+  <ThemeContext.Provider value={[theme, toggleTheme]}>
+    <ThemeProvider theme={themes[theme]}>
+      <Router>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/gradient' component={Gradient} />
+      </Router>
+    </ThemeProvider>
+  </ThemeContext.Provider>
+)
+
+}
+
+export default App
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,81 +1,145 @@
-import React, { useState, useEffect } from "react";
-import injectSheet from "react-jss";
-import FontFaceObserver from 'fontfaceobserver'; 
+import React, { useState, useEffect, useContext } from 'react'
+import FontFaceObserver from 'fontfaceobserver'
+import { createUseStyles, useTheme } from 'react-jss'
+import Toggle from 'react-toggle' 
+import './Toggle.css'
 
-import avatarSrc from "../media/amsterdam.jpeg";
-import { ReactComponent as TelegramIcon } from "../icons/telegram.svg";
-import { ReactComponent as GithubIcon } from "../icons/github.svg";
-import { ReactComponent as MailIcon } from "../icons/mail.svg";
-import { ReactComponent as LinkedinIcon } from "../icons/linkedin.svg";
+import { ThemeContext } from '../context'
+import content from '../constants'
 
-const styles = {
+import alviSrc from '../media/alvi.jpg'
+import tonySrc from '../media/tony.jpg'
+import { ReactComponent as TelegramIcon } from '../icons/telegram.svg'
+import { ReactComponent as GithubIcon } from '../icons/github.svg'
+import { ReactComponent as MailIcon } from '../icons/mail.svg'
+import { ReactComponent as LinkedinIcon } from '../icons/linkedin.svg'
+import { ReactComponent as CatIcon } from '../icons/cat.svg'
+
+const useStyles = createUseStyles(theme => ({
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    backgroundColor: theme.background,
+    color: theme.color,
+    minHeight: '100vh',
+  },
   main: {
-    border: "1px solid black",
-    padding: "20px 40px",
-    "& > *:not(:last-child)": {
-      marginBottom: "20px"
+    width: 'calc(100% - 80px)',
+    maxWidth: '700px',
+    padding: '20px 40px',
+    '& > *:not(:last-child)': {
+      marginBottom: '20px'
     }
   },
   avatar: {
-    borderRadius: "50%",
-    height: "50%",
-    width: "50%"
+    borderRadius: '50%',
+    height: '350px',
+    width: '350px',
   },
   avatarSection: {
-    display: "flex",
-    justifyContent: "center"
+    display: 'flex',
+    justifyContent: 'center',
+    minHeight: '350px',
+    minWidth: '350px',
   },
   title: {
-    fontSize: "3em",
-    textAlign: "center",
+    fontSize: '3em',
+    textAlign: 'center',
     marginTop: 0,
   },
   icons: {
-    display: "flex",
-    justifyContent: "center",
-    "& svg": {
-      "&:hover": {
-        "& path": {
-          fill: "rgba(0,0,0,0.2)"
+    marginTop: '40px',
+    display: 'flex',
+    justifyContent: 'center',
+    '& svg': {
+      '& path': {
+        fill: theme.color,
+      },
+      '&:hover': {
+        '& path': {
+          fill: theme.hover,
         }
       }
     }
-  }
-};
+  },
+  toggleLabel: {
+    display: 'flex',
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+  catIcon: {
+    transform: 'scale(0.75) translate(-5px, -4px)',
+  },
+  article: {
+    fontSize: '20px',
+    marginBottom: '40px',
+    textAlign: 'justify',
+  },
+  '@media (max-width: 350px)': {
+    avatar: {
+      height: '250px',
+      width: '250px',
+    },
+    avatarSection: {
+      minHeight: '250px',
+      minWidth: '250px',
+    },
+    title: {
+      fontSize: '3em',
+    },
+  },
+}))
 
-const Home = ({ classes }) => {
-  const [ isAvatarLoaded, setIsAvatarLoaded ] = useState(false);
-  const [ isFontLoaded, setIfFontLoaded ] = useState(false);
+
+const Home = (props) => {
+  const [isAvatarLoaded, setIsAvatarLoaded] = useState(false)
+  const [isFontLoaded, setIfFontLoaded] = useState(false)
+  const [theme, toggleTheme]  = useContext(ThemeContext)
+  const classes = useStyles(useTheme())
   
   useEffect(() => {
     // prevent content flashes
-    const avatar  = new Image;
-    avatar.onload = () => setIsAvatarLoaded(true);
-    avatar.src = avatarSrc;
+    const alviAvatar  = new Image
+    const tonyAvatar = new Image
+    alviAvatar.onload = () => setIsAvatarLoaded(true)
+    alviAvatar.src = alviSrc
+    tonyAvatar.src = tonySrc
 
-    const font = new FontFaceObserver('Titillium Web');
-    font.load().then(() => setIfFontLoaded(true));
+    const font = new FontFaceObserver('Quicksand')
+    font.load().then(() => setIfFontLoaded(true))
   }, [])
 
   return isAvatarLoaded && isFontLoaded && (
-    <main className={classes.main}>
-    <section className={classes.avatarSection}>
-      <img className={classes.avatar} src={avatarSrc} alt="Alvi Ayubov" />
-    </section>
-    <h1 className={classes.title}>Alvi Ayubov</h1>
-    <article className={classes.textContent}>
-      I'm Alvi. I work as Frontend Developer at Rambler&CO in Moscow. I have a good
-      experience at making apps with modern techniques. I love programming,
-      reading, sport and travelling.
-    </article>
-    <footer className={classes.icons}>
-      <GithubIcon />
-      <LinkedinIcon />
-      <TelegramIcon />
-      <MailIcon />
-    </footer>
-  </main>
+    <div className={classes.container}>
+      <main className={classes.main}>
+        <label className={classes.toggleLabel}>
+          <div className={classes.spacer} />
+          <Toggle
+            defaultChecked={false}
+            icons={{
+              checked: null,
+              unchecked: <CatIcon className={classes.catIcon}/>,
+            }}
+            onChange={toggleTheme} />
+        </label>
+        <section className={classes.avatarSection}>
+        {theme === 'alvi' && <img className={classes.avatar} src={alviSrc} alt='Alvi Ayubov' />}
+        {theme === 'tony' && <img className={classes.avatar} src={tonySrc} alt='Tony Ayubov' />}
+        </section>
+        <h1 className={classes.title}>{content[theme].name}</h1>
+        <article className={classes.article}>
+          {content[theme].description}
+        </article>
+        {theme === 'alvi' && (<footer className={classes.icons}>
+          <GithubIcon />
+          <LinkedinIcon />
+          <TelegramIcon />
+          <MailIcon />
+        </footer>)}
+      </main>
+    </div>
   )
 }
 
-export default injectSheet(styles)(Home);
+export default Home
